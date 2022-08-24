b77b95563088a0dbe655960e487be84bd95ecd6a
atigar <tim.garbe7@gmail.com>
2022-08-24 11:18:30 +0200

    download fix with logging

--- download_data.py
+++ download_data.py
@@ -1,5 +1,8 @@
 import glob
 import os
+from time import sleep
+
+from loguru import logger
 import pandas as pd
 import requests
 
@@ -19,15 +22,41 @@ files = glob.glob('yellow*') + glob.glob('green*') + glob.glob('fhv*')
 It downloads the parquet file, converts it to csv (with compression: 800MB -> 100MB) and then saves it to data/.
 Then it deletes the downloaded parquet file. 
 """
-for x in types:
-    for y in years:
-        for z in months:
-            url_tmp = url_add + x + '_tripdata_' + y + '-' + z + '.parquet'
-            file_tmp = x + '_' + z + '_' + y + '.gz'
-            if (file_tmp not in files) and (requests.head(url_tmp).status_code != 404) and (requests.head(url_tmp).status_code != 403):
-                # Determine number of columns (from data dictionaries)
-                # col_list = list(range(0, 20)) if 'green' in file_tmp else list(range(0, 19))
-                tmp_data = pd.read_parquet(url_tmp)
-                tmp_data.to_csv(file_tmp, compression='gzip')
-                del tmp_data
-            print(file_tmp)
+
+if not os.path.exists('data'):
+    os.mkdir('./data')
+
+# Set the download directory
+os.chdir('data/')
+
+
+def requests_data():
+    # Get list of already downloaded files
+    files = glob.glob('yellow*') + glob.glob('green*') + glob.glob('fhv*')
+
+    for x in types:
+        for y in years:
+            for z in months:
+                url_tmp = url_add + x + '_tripdata_' + y + '-' + z + '.parquet'
+                file_tmp = x + '_' + z + '_' + y + '.parquet'
+
+                if file_tmp not in files:
+                    downloaded = False
+                    while not downloaded:
+                        logger.info(f'try and get {file_tmp}')
+                        r = requests.get(url_tmp)
+                        if r.status_code == 403:
+                            logger.warning('Error 403: waiting a bit..')
+                            sleep(10)
+                            continue
+                        if r.status_code == 404:
+                            logger.info('Error 404: try next.')
+                            break
+                        with open(file_tmp, 'wb') as f:
+                            f.write(r.content)
+                            logger.success(f'Saved {file_tmp}.')
+                            downloaded = True
+
+
+if __name__ == "__main__":
+    requests_data()
