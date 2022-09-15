80aead8f288de0deab79bfb871ee43b440d2e8c6
whoisteve <s.vielland@outlook.de>
2022-09-15 11:57:23 +0200

    add weather data and covid data cleaning and transforming

--- db_init.py
+++ db_init.py
@@ -44,11 +44,40 @@ def prepare_taxi_data():
         df_final = pd.concat([df_final, df_clean])
     return df_final
 
+def prepare_covid_data():
+    subdir = f'.{os.sep}data{os.sep}covid{os.sep}'
+    file = subdir + 'rc75-m7u3.csv'
+
+    df_final = pd.DataFrame()
+
+    df_raw = load.load_covid_data(file)
+    df_clean = transform.clean_covid_data(df_raw)
+    df_final = pd.concat([df_final, df_clean])
+
+    return df_final
+
+
+
+def prepare_weather_data():
+    subdir = f'.{os.sep}data{os.sep}weather{os.sep}'
+    files = glob.glob(f'{subdir}2018*') + glob.glob(f'{subdir}2019*') + glob.glob(f'{subdir}2020*') + glob.glob(
+        f'{subdir}2021*') + glob.glob(f'{subdir}2022*')
+
+    df_final = pd.DataFrame()
+    for file in files:
+        df_raw = load.load_weather_data(file)
+        df_clean = transform.clean_weather_data(df_raw)
+        df_final = pd.concat([df_final, df_clean])
+
+    return df_final
+
 
 if __name__ == '__main__':
     # logger.info('Connect to DB')
     # print(get_meta_info())
     # logger.success('done')
-    df = prepare_taxi_data()
-    df.to_csv('taxi_test2.csv')
+    # df = prepare_taxi_data()
+    # df.to_csv('taxi_test2.csv')
+    # df = prepare_weather_data()
+    df = prepare_covid_data()
     logger.success('u did it')
