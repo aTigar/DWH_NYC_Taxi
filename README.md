9fb8d57e4e8ec0bf04ff3b08aa1026dd17682f2b
whoisteve <s.vielland@outlook.de>
2022-09-17 12:43:32 +0200

    Fix failed db connection

--- README.md
+++ README.md
@@ -4,11 +4,12 @@
 
 ### Anmeldefehler mit `pyodbc`
 
-1. HTWK-VPN aktivieren
-2. `.env` anlegen, Muster aus `.env_sample` übernehmen und ausfüllen
-3. IntelliJ IDEA schließen
-4. Terminal öffnen:
+1. [ODBC-Treiber 18](https://learn.microsoft.com/de-de/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16) installieren
+2. HTWK-VPN aktivieren
+3. `.env` anlegen, Muster aus `.env_sample` übernehmen und ausfüllen
+4. IntelliJ IDEA schließen
+5. Terminal öffnen:
     ```shell
-    runas /netonly /user:htwk\username "C:\Program Files\JetBrains\IntelliJ IDEA 2022.1.1\bin\idea64.exe"
+    runas /netonly /user:htwk\username "C:\Program Files\JetBrains\IntelliJ IDEA <version>\bin\idea64.exe"
     ```
 - Anschließend sollten keine Anmeldefehler entstehen über pyodbc
--- connect_db.py
+++ connect_db.py
@@ -1,31 +1,80 @@
 import os
 from typing import Optional
-
-from dotenv import load_dotenv
 import pyodbc
+import sqlalchemy as sal
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 import pandas as pd
-
-load_dotenv()
-server = os.getenv('SERVER')
-database = os.getenv('DATABASE')
-username = os.getenv('USER')
-password = os.getenv('PASSWORD')
-
-cnxn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password + ';Trusted_Connection=yes;')
-cursor = cnxn.cursor()
+from dotenv import load_dotenv
+from loguru import logger
 
 
-def update_table(df: pd.Dataframe, db_table: str, force: Optional[bool] = False):
-    """ TODO implement
-    Sends cleaned pd.Dataframe to SQL DB.
-    :param df:
-    :param db_table:
-    :param force: set DB table to new df. If false, DB updated from df
-    :return: success or not
-    """
-    pass
+def load_environment():
+    logger.info('Load environment variables...')
+
+    load_dotenv()
+    server = os.getenv('SERVER')
+    database = os.getenv('DATABASE')
+    username = os.getenv('USER')
+    password = os.getenv('PASSWORD')
+
+    logger.success('Environment variables loaded.')
+
+    return username, password, server, database
+
+
+def connect_pyodbc(db_env: tuple):
+    # this alternative is only if sqlalchemy does not work
+    logger.info('Connect to DB with pyodbc...')
+    try:
+        cnxn = pyodbc.connect(
+            'DRIVER={ODBC Driver 18 for SQL Server};SERVER=' + db_env[2] + ';DATABASE=' + db_env[3] + ';UID=' + db_env[
+                0] + ';PWD=' + db_env[1] + ';Trusted_Connection=yes;' + ';TrustServerCertificate=yes;')
+        cursor = cnxn.cursor()
+        # test connection
+        for row in cursor.tables():
+            print(row.table_name)
+        logger.success('Connection to DB was successful')
+    except Exception as e:
+        logger.error('Connection to DB with pyodbc failed')
+
+
+def connect_sqlalchemy(db_env: tuple):
+    logger.info('Connect to DB with SQLAlchemy...')
+    try:
+        connection_url = URL.create(
+            "mssql+pyodbc",
+            username=db_env[0],
+            password=db_env[1],
+            host=db_env[2],
+            port=1433,
+            database=db_env[3],
+            query={
+                "driver": "ODBC Driver 18 for SQL Server",
+                "Trusted_Connection": "yes",
+                "TrustServerCertificate": "yes",
+                # "authentication": "ActiveDirectoryIntegrated",
+            },
+        )
+        engine = create_engine(connection_url)
+        # test connection
+        print(engine.table_names())
+        logger.success('Connection to DB was successful')
+    except Exception as e:
+        logger.error('Connection to DB with SQLAlchemy failed')
+
+
+# def update_table(df: pd.Dataframe, db_table: str, force: Optional[bool] = False):
+#     """ TODO implement
+#     Sends cleaned pd.Dataframe to SQL DB.
+#     :param df:
+#     :param db_table:
+#     :param force: set DB table to new df. If false, DB updated from df
+#     :return: success or not
+#     """
+#     pass
 
 
 if __name__ == '__main__':
-    pass
+    # connect_pyodbc(load_environment())
+    connect_sqlalchemy(load_environment())
